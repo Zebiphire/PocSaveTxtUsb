995c9cabdccaf58425f6058026a3a329bccdea8b
Alexandre KLEIN <alexandre.klein@viacesi.fr>
2019-03-05 06:55:02 +0100

    First

--- PocSaveTxtUsb/PocSaveTxtUsb/MainPage.xaml.cs
+++ PocSaveTxtUsb/PocSaveTxtUsb/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,5 +14,37 @@ namespace PocSaveTxtUsb
         {
             InitializeComponent();
         }
+
+        public string SdFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        //public string SdFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        //public string SdFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        //Write Information to a Local File
+        public void WriteLocalFile(string FileName, string Data)
+        {
+            string filePath = Path.Combine(SdFolderPath, FileName);
+            File.WriteAllText(filePath, Data);
+        }
+        //Load Information from a Local File
+        //public string LoadLocalFile(string FileName)
+        //{
+        //    string filePath = Path.Combine(PersonalFolderPath, FileName);
+        //    if (File.Exists(filePath))
+        //    {
+        //        showtext.Text = File.ReadAllText(filePath);
+        //        return showtext.Text;
+        //    }
+        //    return null;
+        //}
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            WriteLocalFile("SaveToSd.txt", CommentaryEditor.Text);
+        }
+
+        //private void LoadButton_Clicked(object sender, EventArgs e)
+        //{
+        //    LoadLocalFile("Burak.txt");
+        //}
     }
 }
---
+++ PocSaveTxtUsb/PocSaveTxtUsb/Services/ManipulateData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PocSaveTxtUsb.Services
+{
+    class ManipulateData
+    {
+        //public string PersonalFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        //public string SdFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        ////Write Information to a Local File
+        //public void WriteLocalFile(string FileName, string Data)
+        //{
+        //    string filePath = Path.Combine(PersonalFolderPath, FileName);
+        //    File.WriteAllText(filePath, Data);
+        //}
+        ////Load Information from a Local File
+        //public string LoadLocalFile(string FileName)
+        //{
+        //    string filePath = Path.Combine(PersonalFolderPath, FileName);
+        //    if (File.Exists(filePath))
+        //    {
+        //        showtext.Text = File.ReadAllText(filePath);
+        //        return showtext.Text;
+        //    }
+        //    return null;
+        //}
+
+        //private void SaveButton_Clicked(object sender, EventArgs e)
+        //{
+        //    WriteLocalFile("Burak.txt", text.Text);
+        //}
+
+        //private void LoadButton_Clicked(object sender, EventArgs e)
+        //{
+        //    LoadLocalFile("Burak.txt");
+        //}
+    }
+}
